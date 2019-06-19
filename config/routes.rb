b2b72b457702445bcaf528d90fb6cc128272366d
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-19 20:43:12 +0900

    add about to routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root "application#hello"
+
+  get "static_pages/about"
 end
