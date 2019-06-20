fe2a038ac1bda4cf5716f787ae79f81455960a04
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-20 20:14:30 +0900

    Create Contact Page,  add root, add action.  create contact.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
-
   get 'static_pages/help'
-
- root "static_pages#home"
-
   get "static_pages/about"
+  get "static_pages/contact"
 end
