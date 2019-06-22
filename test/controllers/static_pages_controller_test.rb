c01784dac421988cd43355fb2e00762b8ffbad63
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-22 10:06:51 +0900

    update test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,12 +12,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
   test "should get help" do
     get static_pages_help_url
     assert_response :success
