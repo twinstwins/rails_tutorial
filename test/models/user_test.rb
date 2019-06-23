2418d5f13b53a352d3bc022cc01951afe208a067
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-23 13:23:36 +0900

    validate email address with Regex

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,4 +38,13 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
