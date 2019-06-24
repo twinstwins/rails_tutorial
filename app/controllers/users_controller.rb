16a1b1cfde92abf3d297e73bb633b0e1841e1937
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-24 09:16:22 +0900

    in users_controller.rb, create "create action"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,15 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
   end
 end
