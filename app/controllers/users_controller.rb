ed28fb74fb3e110ce76d4459b682815ee5a0a678
Yuji <weto2kakokako@yahoo.co.jp>
2019-06-24 15:46:19 +0900

    リスト 7.21: フォーム送信時にエラーメッセージを表示するためのパーシャル

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new'
     end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
