22ab2cc34b18d5f87f34951734dedf167c598c18
Yuji <weto2kakokako@yahoo.co.jp>
2019-07-29 18:10:19 +0900

    generate controller Microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -73,8 +73,16 @@ class UserTest < ActiveSupport::TestCase
      assert_not @user.valid?
    end
 
-   test "authenticated? should return false for a user with nil digest" do
-   assert_not @user.authenticated?('')
-   end
+   test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
+#   test "authenticated? should return false for a user with nil digest" do
+#   assert_not @user.authenticated?('','')
+#   end
 
 end
